0395fce010f4527603fd8a68ed9b10ae73f76c00
zhdanown <zhdanown@gmail.com>
2020-08-03 15:31:39 +0300

    add map controls

--- app/components/Map.js
+++ app/components/Map.js
@@ -4,6 +4,7 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'; // remove PR
 import NoContentFiller from './NoContentFiller';
 import IconButton from './IconButton';
 import MIcon from 'react-native-vector-icons/MaterialIcons';
+import {COLORS} from '../config';
 
 const styles = StyleSheet.create({
   map: {
@@ -12,22 +13,9 @@ const styles = StyleSheet.create({
 });
 
 export default ({mapHeight, pharmCoordinates, userLocation}) => {
-  console.log('userLocation', userLocation);
-  console.log('pharmCoordinates', pharmCoordinates);
-  const [zoom, setZoom] = React.useState(15);
+  const DEFAULT_ZOOM = 14
   const _map = React.useRef();
-
-  React.useEffect(() => {
-    if (_map.current) {
-      _map.current.animateCamera(
-        {
-          zoom,
-        },
-        {duration: 400},
-      );
-    }
-  }, [zoom]);
-
+  // const currentZoom = Math.round(Math.log(360 / region.longitudeDelta) / Math.LN2);
 
   const [region, setRegion] = React.useState({
     ...pharmCoordinates,
@@ -39,6 +27,43 @@ export default ({mapHeight, pharmCoordinates, userLocation}) => {
     setRegion(newRegion);
   };
 
+  const fitWithUserLocation = () => {
+    if (_map.current) {
+      console.log(pharmCoordinates);
+      _map.current.fitToCoordinates([pharmCoordinates, userLocation], {
+        // animated: false,
+        edgePadding: {top: 50, right: 50, bottom: 50, left: 50},
+      });
+    }
+  };
+
+  const fitToPharm = () => {
+    _map.current.animateCamera(
+      {
+        center: pharmCoordinates,
+        zoom: DEFAULT_ZOOM,
+      },
+      {duration: 400},
+    );
+  };
+
+  const renderMapControls = () => (
+    <View
+      style={{
+        position: 'absolute',
+        bottom: 10,
+        right: 0,
+        left: 0,
+        justifyContent: 'center',
+        flexDirection: 'row',
+      }}>
+      <ButtonGroup>
+        <MapButton iconName="my-location" onPress={fitWithUserLocation} />
+        <MapButton iconName="add-location" onPress={fitToPharm} />
+      </ButtonGroup>
+    </View>
+  );
+
   if (pharmCoordinates)
     return (
       <>
@@ -47,34 +72,45 @@ export default ({mapHeight, pharmCoordinates, userLocation}) => {
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={[styles.map, {flex: 1}]}
           region={region}
-          // initialRegion={region}
+          minZoomLevel={2}
+          maxZoomLevel={18}
           onRegionChangeComplete={changeRegion}>
           <Marker coordinate={pharmCoordinates} />
           {userLocation && <Marker coordinate={userLocation} />}
         </MapView>
-        <View
-          style={{
-            position: 'absolute',
-            bottom: 10,
-            right: 10,
-            height: 100,
-            borderWidth: 1,
-            borderRadius: 5,
-            borderColor: 'steelblue',
-            width: 50,
-          }}>
-          <IconButton
-            style={{alignItems: 'center', height: 50, justifyContent: 'center'}}
-            onPress={() => setZoom(zoom + 1)}>
-            <MIcon name="add" size={36} color="steelblue" />
-          </IconButton>
-          <IconButton
-            style={{alignItems: 'center', height: 50, justifyContent: 'center'}}
-            onPress={() => setZoom(zoom - 1)}>
-            <MIcon name="remove" size={36} color="steelblue" />
-          </IconButton>
-        </View>
+        {renderMapControls()}
       </>
     );
   else return <NoContentFiller text="Координаты аптеки не доступны" />;
 };
+
+function ButtonGroup(props) {
+  return (
+    <View
+      style={{
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: COLORS.PRIMARY,
+        flexDirection: 'row',
+        width: props.children.length * 50,
+        marginHorizontal: 5,
+      }}>
+      {props.children}
+    </View>
+  );
+}
+
+function MapButton({onPress, iconName}) {
+  return (
+    <IconButton
+      style={{
+        alignItems: 'center',
+        height: 50,
+        width: 50,
+        justifyContent: 'center',
+      }}
+      onPress={onPress}>
+      <MIcon name={iconName} size={36} color={COLORS.PRIMARY} />
+    </IconButton>
+  );
+}
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,5 +1,6 @@
 export * from "./callNumber";
 export * from "./sendEmail";
+export * from "./getRoute";
 export * from "./formatDate";
 export * from "./getWordEnding";
 export * from "./getRelativeDateFormat";
