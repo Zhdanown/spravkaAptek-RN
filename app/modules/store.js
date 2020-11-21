1bf5b248683dcb876ee2ff972e0e7d3ae0210b6f
ZhdanowN <zhdanown@gmail.com>
2020-11-21 17:49:54 +0300

    fix: add migration (settings)

--- app/modules/store.js
+++ app/modules/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
@@ -9,10 +9,13 @@ import rootSaga from './rootSaga';
 import rootReducer from './rootReducer';
 import logSearchHistory from './middleware/logSearchHistory';
 import DateTransform from './Transforms';
+import migrations from './migrations';
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  version: 0,
+  migrate: createMigrate(migrations),
   transforms: [DateTransform],
   whitelist: ['settings', 'search'],
 };
