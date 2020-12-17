eb5624d6d22712f3e6854720a17dcad9454937a9
ZhdanowN <zhdanown@gmail.com>
2020-12-17 11:04:39 +0300

    fix: change alert messages when opening links

--- app/utils/callNumber.js
+++ app/utils/callNumber.js
@@ -6,7 +6,7 @@ export const callNumber = phone => {
   Linking.canOpenURL(phoneNumber)
     .then(supported => {
       if (!supported) {
-        Alert.alert('Недоступно');
+        Alert.alert('Ошибка','Не удалось открыть ссылку');
       } else {
         return Linking.openURL(phoneNumber);
       }
